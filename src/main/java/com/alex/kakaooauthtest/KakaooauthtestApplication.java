d3935f6d0af9a11915a21c2a9b492914905c44d4
yxxnghwan <younghwan960@gmail.com>
2022-07-26 20:17:36 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/com/alex/kakaooauthtest/KakaooauthtestApplication.java
@@ -0,0 +1,13 @@
+package com.alex.kakaooauthtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaooauthtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaooauthtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/kakaooauthtest/KakaooauthtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.kakaooauthtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaooauthtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
